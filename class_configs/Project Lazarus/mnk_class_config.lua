16e7eb21bbab98afa43b6556aa35061e583c3b12
AlgarDude <43156286+AlgarDude@users.noreply.github.com>
2025-08-21 13:31:11 -0400

    Update mnk_class_config.lua
    Remove Unused Mappings

--- class_configs/Project Lazarus/mnk_class_config.lua
+++ class_configs/Project Lazarus/mnk_class_config.lua
@@ -16,141 +16,18 @@ local _ClassConfig = {
             "Transcended Fistwraps of Immortality",
             "Fistwraps of Celestial Discipline",
         },
-        ['Coating'] = {
-            "Spirit Drinker's Coating",
-            "Blood Drinker's Coating",
-        },
     },
     ['AbilitySets']     = {
         ['EndRegen'] = {
-            --Timer 13, can't be used in combat
-            "Second Wind", -- Level 72
             "Third Wind",
-            "Fourth Wind",
-            "Respite",
-            "Reprieve",
-            "Rest",
-            "Breather", --Level 101
-        },
-        ['CombatEndRegen'] = {
-            --Timer 13, can be used in combat.
-            "Hiatus", --Level 106
-            "Relax",
-            "Night's Calming",
-            "Convalesce",
+            "Second Wind",
         },
         ['MonkAura'] = {
-            "Disciple's Aura",
             "Master's Aura",
-        },
-        ['Dicho'] = {
-            "Dichotomic Form",
-            "Dissident Form",
-            "Composite Form",
-            "Ecliptic Form",
-            "Reciprocal Form",
-        },
-        ['Drunken'] = {
-            "Drunken Monkey Style",
-        },
-        ['Curse'] = {
-            -- Curse Line - Alternating expansions
-            "Curse of the Thirteen Fingers", -- 103 TBM
-            "Curse of Fourteen Fists",       -- 108 TBM
-            "Curse of Fifteen Strikes",      -- 113 COV
-            "Curse of Sixteen Shadows",      -- 118 NOS
-            "Curse of Seventeen Facets",     -- 123 TOB
+            "Disciple's Aura",
         },
         ['Fang'] = {
             "Dragon Fang",
-            "Zalikor's Fang",
-            "Hoshkar's Fang",
-            "Zlexak's Fang",
-            "Uncia's Fang",
-        },
-        ['Fists'] = {
-            "Buffeting of Fists",
-            "Wheel of Fists",
-            "Whorl of Fists",
-            "Torrent of Fists",
-            "Firestorm of Fists",
-            "Barrage of Fists",
-            "Flurry of Fists",
-        },
-        ['Precision1'] = {
-            "Doomwalker's Precision Strike",
-            "Firewalker's Precision Strike",
-            "Icewalker's Precision Strike",
-            "Bloodwalker's Precision Strike",
-            "Fatewalker's Precision Strike",
-        },
-        ['Precision2'] = {
-            "Doomwalker's Precision Strike",
-            "Firewalker's Precision Strike",
-            "Icewalker's Precision Strike",
-            "Bloodwalker's Precision Strike",
-            "Fatewalker's Precision Strike",
-        },
-        ['Precision3'] = {
-            "Doomwalker's Precision Strike",
-            "Firewalker's Precision Strike",
-            "Icewalker's Precision Strike",
-            "Bloodwalker's Precision Strike",
-            "Fatewalker's Precision Strike",
-        },
-        ['Precision4'] = {
-            "Doomwalker's Precision Strike",
-            "Firewalker's Precision Strike",
-            "Icewalker's Precision Strike",
-            "Bloodwalker's Precision Strike",
-            "Fatewalker's Precision Strike",
-        },
-        ['Precision5'] = {
-            "Doomwalker's Precision Strike",
-            "Firewalker's Precision Strike",
-            "Icewalker's Precision Strike",
-            "Bloodwalker's Precision Strike",
-            "Fatewalker's Precision Strike",
-        },
-        ['Shuriken'] = {
-            "Vigorous Shuriken",
-        },
-        ['CraneStance'] = {
-            "Crane Stance",
-            "Heron Stance",
-        },
-        ['Synergy'] = {
-            "Fatewalker's Synergy",  -- LS 125
-            "Bloodwalker's Synergy", -- TOL 120
-            "Calanin's Synergy",
-            "Dreamwalker's Synergy",
-            "Veilwalker's Synergy",
-            "Shadewalker's Synergy",
-            "Doomwalker's Synergy",
-            "Firewalker's Synergy",
-            "Icewalker's Synergy",
-        },
-        ['Alliance'] = {
-            -- Alliance line - Alternates expansions
-            "Doomwalker's Alliance",
-            "Firewalker's Covenant",
-            "Icewalker's Coalition",     -- COV
-            "Bloodwalker's Conjunction", -- NOS
-            "Fatewalker's Covariance",   -- TOB
-        },
-        ['Storm'] = {
-            "Eye of the Storm",
-        },
-        ['Breaths'] = {
-            --- Breaths Endurance Line
-            "Five Breaths",
-            "Six Breaths",
-            "Seven Breaths",
-            "Eight Breaths",
-            "Nine Breaths",
-            "Breath of Tranquility",
-            "Breath of Stillness",
-            "Moment of Stillness",
         },
         ['FistsOfWu'] = {
             --- Fists of Wu - Double Attack
@@ -159,45 +36,15 @@ local _ClassConfig = {
         ['EarthDisc'] = {
             -- EarthDisc - Melee Mitigation
             "Earthwalk Discipline",
-            "Earthforce Discipline",
-        },
-        ['ShadedStep'] = {
-            -- ShadedStep - Dodge Bonus 18 Seconds
-            "Void Step",
-            "Shaded Step",
-        },
-        ['RejectDeath'] = {
-            "Repeal Death",
-            "Delay Death",
-            "Defer Death",
-            "Deny Death",
-            "Decry Death",
-            "Forestall Death",
-            "Refuse Death",
-            "Reject Death",
-            "Rescind Death",
-            "Defy Death",
-        },
-        ['DodgeBody'] = {
-            "Void Body",
-            "Veiled Body",
-        },
-        ['MezSpell'] = {
-            "Echo of Disorientation",
-            "Echo of Flinching",
-            "Echo of Diversion",
         },
         ['FistDisc'] = {
             "Ashenhand Discipline",
             "Scaledfist Discipline",
-            "Ironfist Discipline",
         },
         ['Heel'] = {
             "Rapid Kick Discipline",
             "Heel of Kanji",
             "Heel of Kai",
-            "Heel of Kojai",
-            "Heel of Zagali",
         },
         ['Speed'] = {
             "Hundred Fists Discipline",
@@ -206,14 +53,6 @@ local _ClassConfig = {
         ['Palm'] = {
             "Innerflame Discipline",
             "Crystalpalm Discipline",
-            "Diamondpalm Discipline",
-            "Terrorpalm Discipline",
-        },
-        ['Poise'] = {
-            "Dragon's Poise",
-            "Tiger's Poise",
-            "Eagle's Poise",
-            "Tiger's Symmetry",
         },
     },
     ['HelperFunctions'] = {
